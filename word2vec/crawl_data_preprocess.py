01e33a639002c26783d7fbb4173517679de54573
Pranav Ghate <pranavghate94@gmail.com>
2017-03-29 20:11:52 -0700

    Changed crawl_data_preprocess.py to parse newly crawled news website data

--- word2vec/crawl_data_preprocess.py
+++ word2vec/crawl_data_preprocess.py
@@ -9,6 +9,16 @@ from preprocess import Preprocess_Text
 class Preprocess_Crawl_Text(Preprocess_Text):
     def __init__(self,directory_name='../../Data',temp_results='../../temp_results',raw_file_name='crawled_news_text.txt'):
         Preprocess_Text.__init__(self,directory_name,temp_results,raw_file_name)
+
+
+    def parse_line_article(self, line):
+        line = line.split("#|#")
+        if(len(line)<2 or not line[0]):
+            return (None, None, False)
+        else:
+            headline = line[0]
+            text = line[1]
+        return (headline,text,True)
         
     def generate_crawl_raw_file(self,):
         count = 0 
@@ -23,9 +33,14 @@ class Preprocess_Crawl_Text(Preprocess_Text):
                     file_path = os.path.join(root, file_name)
                     with codecs.open(file_path, encoding="utf-8") as each_file_pointer:
                         for each_line in each_file_pointer:
-                            headline_tokens = self.tokenize(each_line)
-                            single_news = u" ".join(headline_tokens) + u"\n"
-                            f.write(single_news)
+                            headline, text , has_headline= self.parse_line_article(each_line)
+                            if has_headline:
+                                headline_tokens = self.tokenize(headline)
+                                text_tokens = self.tokenize(text)
+                                single_news_article = u" ".join(headline_tokens) + u"#|#" + u" ".join(text_tokens) + "\n"
+                                f.write(single_news_article)
+                            else:
+                                print("No headline in line: " + str(count))
                             count = count + 1
                             if count%10000==0:
                                 print ("Processing done till ",count, "time took ",time.time()-start_time)
