b982a9befefe0039010e66adf41c40706b1f2f54
Pranav Ghate <pranavghate94@gmail.com>
2017-04-05 18:58:40 -0700

    Checks for empty headlines

--- word2vec/crawl_data_preprocess.py
+++ word2vec/crawl_data_preprocess.py
@@ -13,15 +13,15 @@ class Preprocess_Crawl_Text(Preprocess_Text):
 
     def parse_line_article(self, line):
         line = line.split("#|#")
-        if(len(line)<2 or not line[0]):
-            return (None, None, False)
-        else:
-            headline = line[0]
-            text = line[1]
-        return (headline,text,True)
+        if(len(line)<2 or len(line[0])==0 or len(line[1])==0):
+        	return (None, None)        
+        headline = line[0]
+        text = line[1]
+        return (headline,text)
         
     def generate_crawl_raw_file(self,):
         count = 0 
+        malformed_articles= 0
         start_time = time.time()
         with codecs.open(self.raw_file_name, "w", encoding="utf-8") as f:
             for root, subdirs, files in os.walk(self.directory_name):
@@ -33,17 +33,18 @@ class Preprocess_Crawl_Text(Preprocess_Text):
                     file_path = os.path.join(root, file_name)
                     with codecs.open(file_path, encoding="utf-8") as each_file_pointer:
                         for each_line in each_file_pointer:
-                            headline, text , has_headline= self.parse_line_article(each_line)
-                            if has_headline:
+                            headline, text = self.parse_line_article(each_line)
+                            if headline and text:
                                 headline_tokens = self.tokenize(headline)
                                 text_tokens = self.tokenize(text)
                                 single_news_article = u" ".join(headline_tokens) + u"#|#" + u" ".join(text_tokens) + "\n"
                                 f.write(single_news_article)
                             else:
-                                print("No headline in line: " + str(count))
+                                malformed_articles = malformed_articles + 1
                             count = count + 1
                             if count%10000==0:
                                 print ("Processing done till ",count, "time took ",time.time()-start_time)
+        print ("Total malformed articles ",malformed_articles)
 
 def main():
     process_crawl_data = Preprocess_Crawl_Text()
